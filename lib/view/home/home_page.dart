cd7759e9c393e5123ae2550083bb3908a1e9e837
jamshidisoqov <isoqovjamshid01@gmail.com>
2023-02-15 11:22:23 +0500

    bottom navigation added

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -14,9 +14,21 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
         items: const [
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.home)),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.bookmark)),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.person)),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: "Home",
+            activeIcon: Icon(CupertinoIcons.house_fill),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.bookmark),
+            label: "Saved",
+            activeIcon: Icon(CupertinoIcons.bookmark_fill),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person),
+            label: "Profile",
+            activeIcon: Icon(CupertinoIcons.person_fill),
+          ),
         ],
       ),
     );
